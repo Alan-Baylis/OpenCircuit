161f8c62d9db59e74c8dedabf4b6b6bd29d21cdf
michbr <bmichaud45@gmail.com>
2016-03-23 13:13:31 -0600

    Play a sound on ammo pickup

--- Assets/Scripts/Items/AmmoPickup.cs
+++ Assets/Scripts/Items/AmmoPickup.cs
@@ -5,15 +5,31 @@ using System.Collections;
 public class AmmoPickup : NetworkBehaviour {
 
 	public int magazines = 1;
+	public AudioClip pickupSound;
+
+	private AudioSource soundEmitter;
+	private bool pickedUp = false;
+
+	void Start() {
+		soundEmitter = gameObject.AddComponent<AudioSource>();
+
+	}
+
 
 	[ServerCallback]
 	public void OnTriggerEnter(Collider other) {
-		Player player = other.GetComponent<Player>();
-		if(player != null) {
-			AbstractGun gun = player.GetComponentInChildren<AbstractGun>();
-			if(gun != null) {
-				if(gun.addMags(magazines)) {
-					Destroy(gameObject);
+		if(!pickedUp) {
+			Player player = other.GetComponent<Player>();
+			if(player != null) {
+				AbstractGun gun = player.GetComponentInChildren<AbstractGun>();
+				if(gun != null) {
+					if(gun.addMags(magazines)) {
+						soundEmitter.PlayOneShot(pickupSound);
+						//gameObject.SetActive(false);
+						pickedUp = true;
+						Destroy(GetComponent<MeshRenderer>());
+						Destroy(gameObject, 5);
+					}
 				}
 			}
 		}
