f7bb8cff38fd0f6416a821109e5b0d2db3ff4886
michbr <bmichaud45@gmail.com>
2016-03-30 12:33:57 -0600

    Make landmines work over the network

--- Assets/Scripts/LandMine.cs
+++ Assets/Scripts/LandMine.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class LandMine : MonoBehaviour {
+public class LandMine : NetworkBehaviour {
 
 	public float knockBack = 5f;
 	public float damage = 70f;
@@ -15,25 +16,37 @@ public class LandMine : MonoBehaviour {
 		soundEmitter.volume = 1f;
 	}
 
+	[ServerCallback]
 	public void OnTriggerEnter(Collider other) {
 		if(!triggered) {
 			Player player = other.GetComponent<Player>();
 			if(player != null) {
 				triggered = true;
-				if(explosionSound != null) {
-					soundEmitter.PlayOneShot(explosionSound);
-				}
+
 				player.GetComponent<Label>().sendTrigger(this.gameObject, new DamageTrigger(damage));
 				Vector3 horizontalDirection = (player.transform.position - transform.position);
 				Vector3 verticalDirection = new Vector3(0, 1, 0);
 				horizontalDirection.y = 0;
 				horizontalDirection.Normalize();
 				player.GetComponent<Rigidbody>().AddForce(horizontalDirection * knockBack + verticalDirection * (knockBack * .3f));
-				Destroy(GetComponent<MeshRenderer>());
-				Destroy(GetComponent<MeshFilter>());
+				handleEffects(player);
+				RpcHandleEffects(player.netId);
 				Destroy(gameObject, 3f);
 
 			}
 		}
+	}
+
+	[ClientRpc]
+	private void RpcHandleEffects(NetworkInstanceId id) {
+		handleEffects(ClientScene.FindLocalObject(id).GetComponent<Player>());
+	}
+
+	private void handleEffects(Player player) {
+		if(explosionSound != null) {
+			soundEmitter.PlayOneShot(explosionSound);
+		}
+		Destroy(GetComponent<MeshRenderer>());
+		Destroy(GetComponent<MeshFilter>());
 	}
 }
