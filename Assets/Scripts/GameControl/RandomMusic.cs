314510eb86369a5ce3e2f72c58f844fa39a853cb
michbr <bmichaud45@gmail.com>
2016-04-22 23:27:10 -0600

    Wait to start the music until the robots are coming...hehehe }:)

--- Assets/Scripts/GameControl/RandomMusic.cs
+++ Assets/Scripts/GameControl/RandomMusic.cs
@@ -6,6 +6,7 @@ public class RandomMusic : MonoBehaviour {
 	private float nextTime;
 	private AudioSource source;
 	private AudioClip last;
+	private bool isActive = false;
 
 	public int minTimeWait;
 	public int maxTimeWait;
@@ -13,7 +14,7 @@ public class RandomMusic : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		nextTime = Time.fixedTime + Random.Range(minTimeWait, maxTimeWait);
+		nextTime = Time.fixedTime;// + Random.Range(minTimeWait, maxTimeWait);
 		source = gameObject.GetComponent<AudioSource>();
 		if (source == null)
 			source = gameObject.AddComponent<AudioSource>();
@@ -21,17 +22,27 @@ public class RandomMusic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (nextTime < Time.fixedTime) {
-			for (int i = 0; i<10; ++i) {
-				AudioClip newTrack = tracks[Random.Range(0, tracks.Length)];
-				if (newTrack != last) {
-					last = newTrack;
-					break;
+		if(isActive) {
+			if(nextTime < Time.fixedTime) {
+				for(int i = 0; i < 10; ++i) {
+					AudioClip newTrack = tracks[Random.Range(0, tracks.Length)];
+					if(newTrack != last) {
+						last = newTrack;
+						break;
+					}
 				}
-            }
-			source.clip = last;
-			source.Play();
-			nextTime = Time.fixedTime + Random.Range(minTimeWait, maxTimeWait) +source.clip.length;
+				source.clip = last;
+				source.Play();
+				nextTime = Time.fixedTime + Random.Range(minTimeWait, maxTimeWait) + source.clip.length;
+			}
 		}
+	}
+
+	public void OnTriggerEnter(Collider other) {
+		Player player = other.gameObject.GetComponent<Player>();
+		if(player != null) {
+			isActive = true;
+		}
+		
 	}
 }
