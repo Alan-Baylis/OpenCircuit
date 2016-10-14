688ffe178c83eee840175230964dad10d2e4d723
semimono <culleneason@gmail.com>
2016-10-14 14:05:15 -0600

    Added readonly attribute.

---
+++ Assets/Scripts/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using System.ComponentModel;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer {
+
+	public override void OnGUI(Rect position,
+							   SerializedProperty property,
+							   GUIContent label) {
+		Object obj = property.serializedObject.targetObject;
+		if (PrefabUtility.GetPrefabType(obj) != PrefabType.Prefab
+			&& Application.isPlaying)
+			GUI.enabled = false;
+		EditorGUI.PropertyField(position, property, label, true);
+		GUI.enabled = true;
+	}
+}
