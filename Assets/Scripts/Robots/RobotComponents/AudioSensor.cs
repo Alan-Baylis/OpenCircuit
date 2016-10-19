a26dcd755b50a59f2d768f58ba1a42b40dfd1b54
semimono <culleneason@gmail.com>
2016-10-19 13:09:24 -0600

    Made AudioSensor.cs more efficient.

--- Assets/Scripts/Robots/RobotComponents/AudioSensor.cs
+++ Assets/Scripts/Robots/RobotComponents/AudioSensor.cs
@@ -7,7 +7,7 @@ public class AudioSensor : AbstractRobotComponent {
 	public float range = 30f; 
 	private bool hasPower;
 
-    public static List<AudioSensor> sensors = new List<AudioSensor>();
+    public static HashSet<AudioSensor> sensors = new HashSet<AudioSensor>();
 
 	public void processAudioEvent(AudioEvent eventMessage) {
 		if(hasPower) {
