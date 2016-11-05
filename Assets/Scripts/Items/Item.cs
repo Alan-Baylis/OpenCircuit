b5f8ef3ce293986a0a4a9f17887fb99951150a6f
michbr <bmichaud45@gmail.com>
2016-11-05 13:47:57 -0600

    Parent equipped items correctly for all clients

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 using UnityEngine.Networking;
-using System.Collections;
-using System.Collections.Generic;
 
 public abstract class Item : NetworkBehaviour {
 
-    public Texture2D icon;
+	public Texture2D icon;
 	public HoldPosition normalPosition;
 	public HoldPosition sprintPosition;
 	public HoldPosition zoomPosition;
@@ -21,26 +19,42 @@ public abstract class Item : NetworkBehaviour {
 	protected Collider col;
 	private Transform myFollowingCamera;
 
-	protected Transform followingCamera {
-		get {
-			if(holder != null && myFollowingCamera == null) {
+	protected Transform followingCamera
+	{
+		get
+		{
+			if (holder != null && myFollowingCamera == null) {
 				myFollowingCamera = holder.getPlayer().cam.transform;
 			}
-			return myFollowingCamera; }
+			return myFollowingCamera;
+		}
 		set { myFollowingCamera = value; }
 	}
 	protected bool rightStepNext;
 
+	[SyncVar]
 	private NetworkInstanceId parent;
 
 	public void Awake() {
 		col = GetComponent<Collider>();
 	}
 
+	public override void OnStartClient() {
+		base.OnStartClient();
+		if (parent != null) {
+			GameObject parentObj = ClientScene.FindLocalObject(this.parent);
+			if (parentObj != null) {
+				Inventory inventory = parentObj.GetComponent<Inventory>();
+				if (inventory != null) {
+					inventory.take(this);
+				}
+			}
+		}
+	}
+
 	public void onTake(NetworkInstanceId parent) {
 		this.parent = parent;
 		transform.parent = ClientScene.FindLocalObject(this.parent).transform;
-		Inventory inventory = transform.parent.GetComponent<Inventory>();
 	}
 
 	public virtual void Update() {
@@ -59,7 +73,7 @@ public abstract class Item : NetworkBehaviour {
 	}
 
 	public virtual void doStep(float strength) {
-		Vector3 nextStepBump = stepBump *strength;
+		Vector3 nextStepBump = stepBump * strength;
 		if (!rightStepNext)
 			nextStepBump.x = -nextStepBump.x;
 		transform.position += transform.TransformVector(nextStepBump);
@@ -79,29 +93,29 @@ public abstract class Item : NetworkBehaviour {
 
 	public abstract void beginInvoke(Inventory invoker);
 
-	public virtual void endInvoke(Inventory invoker) {}
+	public virtual void endInvoke(Inventory invoker) { }
 
-    public virtual void onTake(Inventory taker) {
-        transform.SetParent(taker.transform);
+	public virtual void onTake(Inventory taker) {
+		transform.SetParent(taker.transform);
 		holder = taker;
-        gameObject.SetActive(false);
-    }
+		gameObject.SetActive(false);
+	}
 
-    public virtual void onDrop(Inventory taker) {
-        transform.SetParent(null);
+	public virtual void onDrop(Inventory taker) {
+		transform.SetParent(null);
 		myFollowingCamera = null;
 		gameObject.SetActive(true);
 		endInvoke(taker);
 		holder = null;
 	}
 
-    public virtual void onEquip(Inventory equipper) {
+	public virtual void onEquip(Inventory equipper) {
 		transform.localPosition = normalPosition.position;
 		col.enabled = false;
 		gameObject.SetActive(true);
-    }
+	}
 
-    public virtual void onUnequip(Inventory equipper) {
+	public virtual void onUnequip(Inventory equipper) {
 		gameObject.SetActive(false);
 		endInvoke(equipper);
 		col.enabled = true;
@@ -111,11 +125,11 @@ public abstract class Item : NetworkBehaviour {
 	protected RaycastHit reach(out Vector3 position) {
 		RaycastHit finalHit = new RaycastHit();
 		position = Vector3.zero;
-		Ray ray = new Ray (this.transform.parent.position, this.transform.parent.forward);
+		Ray ray = new Ray(this.transform.parent.position, this.transform.parent.forward);
 		RaycastHit[] hits = Physics.RaycastAll(ray, reachDistance);
 		float distance = reachDistance;
 
-		foreach(RaycastHit hit in hits) {
+		foreach (RaycastHit hit in hits) {
 			if (hit.distance > distance) continue;
 			if (hit.collider.isTrigger) continue;
 			finalHit = hit;
@@ -133,15 +147,15 @@ public abstract class Item : NetworkBehaviour {
 			newRotation = followingCamera.rotation * Quaternion.Euler(position.rotation);
 		} else {
 			Quaternion rotation = Quaternion.Euler(0, followingCamera.localEulerAngles.y, 0);
-			newPosition = transform.parent.TransformPoint(rotation *position.position);
+			newPosition = transform.parent.TransformPoint(rotation * position.position);
 			newRotation = transform.parent.rotation * Quaternion.Euler(position.rotation);
 			newRotation = rotation * newRotation;
 		}
 
 		// track to desired position
-		float posResponse = Mathf.Min(positionalResponsiveness *Time.deltaTime *responsivenessMultiplier, 1);
-		float rotResponse = Mathf.Min(responsiveness *Time.deltaTime *responsivenessMultiplier, 1);
-		transform.position += (newPosition -transform.position) *posResponse;
+		float posResponse = Mathf.Min(positionalResponsiveness * Time.deltaTime * responsivenessMultiplier, 1);
+		float rotResponse = Mathf.Min(responsiveness * Time.deltaTime * responsivenessMultiplier, 1);
+		transform.position += (newPosition - transform.position) * posResponse;
 		transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, rotResponse);
 	}
 
