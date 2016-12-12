d20792d567a273f15658ed9c2ba2a7656a858970
michbr <bmichaud45@gmail.com>
2016-12-12 12:24:52 -0700

    Add the InvestigateLostPlayer Endeavour

---
+++ Assets/Scripts/Attributes/Endeavour/Factories/InvestigateLostPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class InvestigateLostPlayer : EndeavourFactory {
+
+	private static List<TagRequirement> requiredTags = new List<TagRequirement> { new TagRequirement(TagEnum.Player, true) };
+
+	protected override Endeavour createEndeavour(RobotController controller, Dictionary<TagEnum, Tag> tagMap) {
+		return new InvestigateLostPlayerAction(this, controller, goals, tagMap);
+	}
+
+	public static new List<TagRequirement> getRequiredTags() {
+		return requiredTags;
+	}
+}
---
+++ Assets/Scripts/Attributes/Endeavour/InvestigateLostPlayerAction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class InvestigateLostPlayerAction : Endeavour {
+
+	private Tag player;
+
+	public InvestigateLostPlayerAction(EndeavourFactory factory, RobotController controller, List<Goal> goals, Dictionary<TagEnum, Tag> tags) : base(factory, controller, goals, tags) {
+		this.name = "investigateListPlayer";
+		this.player = getTagOfType<Tag>(TagEnum.Player);
+	}
+
+	public override bool canExecute() {
+		return true;
+	}
+
+	public override TagEnum getPrimaryTagType() {
+		return TagEnum.Player;
+	}
+
+	public override System.Type[] getRequiredComponents() {
+		return new System.Type[] { typeof(HoverJet) };
+	}
+
+	public override bool isStale() {
+		return false;
+	}
+
+	public override bool singleExecutor() {
+		return false;
+	}
+
+	protected override float getCost() {
+		return getController().getRobotComponent<HoverJet>().calculatePathCost(getController().getMentalModel().getLastKnownPosition(player.getLabelHandle()).Value);
+	}
+
+	protected override void onExecute() {
+		HoverJet jet = getController().getRobotComponent<HoverJet>();
+				
+	}
+}
