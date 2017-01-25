3bfa8bef76e601e5c379895563a48cd5a6a8bd2e
michbr <bmichaud45@gmail.com>
2017-01-25 12:50:19 -0700

    Create an endeavour for building towers

---
+++ Assets/Scripts/Attributes/Endeavour/Factories/BuildTower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BuildTower : EndeavourFactory {
+
+	private static List<TagRequirement> requiredTags = new List<TagRequirement> { new TagRequirement(TagEnum.BuildDirective, false) };
+
+	protected override Endeavour createEndeavour(RobotController controller, Dictionary<TagEnum, Tag> tagMap) {
+		return new BuildTowerAction();
+	}
+
+	public static new List<TagRequirement> getRequiredTags() {
+		return requiredTags;
+	}
+}
