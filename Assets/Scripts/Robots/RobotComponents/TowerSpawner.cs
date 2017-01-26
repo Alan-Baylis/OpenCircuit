c8053f94c61d61fe5fbf3f59bb85bf0f8d4e6158
michbr <bmichaud45@gmail.com>
2017-01-26 12:01:31 -0700

    Add missed files.

---
+++ Assets/Scripts/Robots/RobotComponents/TowerSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class TowerSpawner : AbstractRobotComponent {
+
+	public GameObject towerPrefab;
+
+	[Server]
+	public GameObject buildTower(Vector3 position) {
+		GameObject newTower = GameObject.Instantiate(towerPrefab, position, towerPrefab.transform.rotation) as GameObject;
+		NetworkServer.Spawn(newTower);
+		return newTower;
+	}
+}
