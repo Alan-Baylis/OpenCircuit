f689c8b1e147dad7305d284104155d4cc2b78853
michbr <bmichaud45@gmail.com>
2017-01-26 12:05:54 -0700

    Destroy the tower base when the tower is built and use an offset to correct height

--- Assets/Scripts/Robots/RobotComponents/TowerSpawner.cs
+++ Assets/Scripts/Robots/RobotComponents/TowerSpawner.cs
@@ -4,10 +4,12 @@ using UnityEngine.Networking;
 public class TowerSpawner : AbstractRobotComponent {
 
 	public GameObject towerPrefab;
+	public Vector3 offset;
+
 
 	[Server]
 	public GameObject buildTower(Vector3 position) {
-		GameObject newTower = GameObject.Instantiate(towerPrefab, position, towerPrefab.transform.rotation) as GameObject;
+		GameObject newTower = GameObject.Instantiate(towerPrefab, position + offset, towerPrefab.transform.rotation) as GameObject;
 		NetworkServer.Spawn(newTower);
 		return newTower;
 	}
