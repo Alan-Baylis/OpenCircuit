3a1a68b0d2b48c918d664d887fea88d6e6de20f7
michbr <bmichaud45@gmail.com>
2017-03-11 13:39:06 -0700

    Initial work on teams

--- Assets/Scripts/GameControl/GameModes/Bases.cs
+++ Assets/Scripts/GameControl/GameModes/Bases.cs
@@ -3,7 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bases : GameMode {
+public class Bases : TeamGameMode {
+
+    void Start() {
+        teams.Add(new TeamData(0, new Color(0, 0, .4777f, 1)));
+        teams.Add(new TeamData(1, new Color(.4777f, 0, 0, 1)));
+    }
+
 	public override bool winConditionMet() {
 		return false;
 	}
