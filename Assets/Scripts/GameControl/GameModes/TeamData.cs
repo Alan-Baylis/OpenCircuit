297bf24cceb9ac7c7d5b7ceb278818c97b2079e8
michbr <bmichaud45@gmail.com>
2017-03-11 13:39:06 -0700

    Initial work on teams

---
+++ Assets/Scripts/GameControl/GameModes/TeamData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct TeamData {
+    public int Id;
+    public Color color;
+
+    public TeamData(int ID, Color color) {
+        Id = ID;
+        this.color = color;
+    }
+}
---
+++ Assets/Scripts/GameControl/GameModes/TeamGameMode.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class TeamGameMode : GameMode {
+
+    public List<TeamData> teams = new List<TeamData>();
+
+}
