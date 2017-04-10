d11920ea2fa235c3ede8e241405b02e19e88a67b
michbr <bmichaud45@gmail.com>
2017-04-10 11:02:05 -0600

    Simplify tower spawn code

--- Assets/Scripts/Robots/RobotComponents/TowerSpawner.cs
+++ Assets/Scripts/Robots/RobotComponents/TowerSpawner.cs
@@ -4,34 +4,26 @@ using UnityEngine.Networking;
 public class TowerSpawner : AbstractRobotComponent {
 
 	public GameObject towerPrefab;
-	public GameObject towerTurretPrefab;
-	public GameObject eyesPrefab;
-	public GameObject generatorPrefab;
+	public GameObject[] components;
 	public Vector3 offset;
 
 
 	[Server]
 	public GameObject buildTower(Vector3 position) {
 		GameObject newTower = Instantiate(towerPrefab, position + offset, towerPrefab.transform.rotation);
-		GameObject towerTurret = Instantiate(towerTurretPrefab, position+offset+towerTurretPrefab.transform.position,
-			towerTurretPrefab.transform.rotation, newTower.transform);
-		GameObject eyes = Instantiate(eyesPrefab, position+offset+eyesPrefab.transform.position,
-			eyesPrefab.transform.rotation, newTower.transform);
-		GameObject gen = Instantiate(generatorPrefab, position+offset+generatorPrefab.transform.position,
-			generatorPrefab.transform.rotation, newTower.transform);
 
 		RobotController controller = newTower.GetComponent<RobotController>();
-		towerTurret.GetComponent<NetworkParenter>().setParentId(controller.netId);
-		eyes.GetComponent<NetworkParenter>().setParentId(controller.netId);
-		gen.GetComponent<NetworkParenter>().setParentId(controller.netId);
-
 		NetworkServer.Spawn(newTower);
-		NetworkServer.Spawn(towerTurret);
-		NetworkServer.Spawn(eyes);
-		NetworkServer.Spawn(gen);
+
+		foreach (GameObject componentPrefab in components) {
+			GameObject component = Instantiate(componentPrefab, position + offset + componentPrefab.transform.position, componentPrefab.transform.rotation);
+			component.GetComponent<NetworkParenter>().setParentId(controller.netId);
+			NetworkServer.Spawn(component);
+
+		}
 
 		if (getController().GetComponent<Team>().enabled && GlobalConfig.globalConfig.gamemode is Bases) {
-			Bases gameMode = ((Bases)GlobalConfig.globalConfig.gamemode);
+			Bases gameMode = (Bases)GlobalConfig.globalConfig.gamemode;
 			int teamId = getController().GetComponent<Team>().team.Id;
 			gameMode.getCRC(teamId).forceAddListener(controller);
 			Team team = newTower.GetComponent<Team>();
