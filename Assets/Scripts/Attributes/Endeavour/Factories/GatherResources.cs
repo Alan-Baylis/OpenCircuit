8273663ade7e7b8704180e1ef9feb9b426815893
michbr <bmichaud45@gmail.com>
2017-04-11 12:06:50 -0600

    Initial work on resources

---
+++ Assets/Scripts/Attributes/Endeavour/Factories/GatherResources.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class GatherResources : EndeavourFactory {
+
+	private static List<TagRequirement> requiredTags = new List<TagRequirement> { new TagRequirement(TagEnum.Resource) };
+
+	protected override Endeavour createEndeavour(RobotController controller, Dictionary<TagEnum, Tag> tagMap) {
+		return new GatherResourcesAction(this, controller, goals, tagMap);
+	}
+
+	public new static List<TagRequirement> getRequiredTags() {
+		return requiredTags;
+	}
+}
---
+++ Assets/Scripts/Attributes/Endeavour/GatherResourcesAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+public class GatherResourcesAction : Endeavour {
+
+	private Tag target;
+
+	public GatherResourcesAction(EndeavourFactory parentFactory, RobotController controller, List<Goal> goals, Dictionary<TagEnum, Tag> tagMap) : base(parentFactory, controller, goals, tagMap) {
+		target = getTagOfType<Tag>(TagEnum.Resource);
+		name = "gatherResources";
+	}
+
+	public override bool isStale() {
+		return !controller.knowsTarget(target.getLabelHandle());
+	}
+
+	protected override void onExecute() {
+		jet.setTarget(target.getLabelHandle(), true);
+		arms.setTarget(target.getLabelHandle().label);
+
+	}
+
+	public override System.Type[] getRequiredComponents() {
+		return new [] { typeof(HoverJet), typeof(AbstractArms)};
+	}
+
+	public override bool canExecute() {
+		return jet.canReach(target.getLabelHandle().label);
+	}
+
+	public override bool singleExecutor() {
+		return true;
+	}
+
+	public override TagEnum getPrimaryTagType() {
+		return TagEnum.Resource;
+	}
+
+	protected override float getCost() {
+		return jet.calculatePathCost(target.getLabelHandle().getPosition());
+	}
+}
