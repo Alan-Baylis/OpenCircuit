c4c64408268d5206056e136c87c78593d5495eaf
michbr <bmichaud45@gmail.com>
2017-04-27 12:52:29 -0600

    Fix NameTag bug

--- Assets/Scripts/GUI/NameTag.cs
+++ Assets/Scripts/GUI/NameTag.cs
@@ -6,6 +6,16 @@ public class NameTag : NetworkBehaviour {
 	[SyncVar(hook = "onSetName")]
 	public string displayName;
 
+	private Player myPlayer;
+
+	private Player player {
+		get {
+			if (myPlayer == null)
+				myPlayer = GetComponent<Player>();
+			return myPlayer;
+		}
+	}
+
 	public override void OnStartClient() {
 		base.OnStartClient();
 		if (!string.IsNullOrEmpty(displayName)) {
@@ -15,32 +25,35 @@ public class NameTag : NetworkBehaviour {
 
 	[ClientCallback]
 	void OnGUI() {
-		Camera cam = Camera.current;
-		Vector3 pos;
-		if(cam != null) {
-			Vector3 worldTextPos = transform.position + new Vector3(0, 1, 0);
-			pos = cam.WorldToScreenPoint(worldTextPos);
-			if(Vector3.Dot(cam.transform.forward, (worldTextPos - cam.transform.position).normalized) < 0) {
+		if (!player.clientController.isLocalPlayer) {
+			Camera cam = Camera.current;
+			Vector3 pos;
+			if (cam != null) {
+				Vector3 worldTextPos = transform.position + new Vector3(0, 1, 0);
+				pos = cam.WorldToScreenPoint(worldTextPos);
+				if (Vector3.Dot(cam.transform.forward, (worldTextPos - cam.transform.position).normalized) < 0) {
+					return;
+				}
+			} else {
 				return;
 			}
-		} else {
-			return;
+
+
+			Font font = Resources.Load<Font>("Fonts/Courier.ttf");
+			GUIStyle style = new GUIStyle(GUI.skin.label);
+			style.font = font;
+			style.fontSize = 14;
+			Vector2 labelSize = style.CalcSize(new GUIContent(displayName));
+
+			Rect textCentered = new Rect(pos.x - labelSize.x / 2, Screen.height - pos.y - labelSize.y + 4, labelSize.x,
+				labelSize.y);
+			GUI.Label(textCentered, displayName, style);
 		}
-
-
-		Font font =	Resources.Load<Font>("Fonts/Courier.ttf");
-		GUIStyle style = new GUIStyle(GUI.skin.label);
-		style.font = font;
-		style.fontSize = 14;
-		Vector2 labelSize = style.CalcSize(new GUIContent(displayName));
-
-		Rect textCentered = new Rect(pos.x - labelSize.x / 2, Screen.height - pos.y - labelSize.y + 4, labelSize.x, labelSize.y);
-		GUI.Label(textCentered, displayName, style);
 	}
 
 	private void onSetName(string name) {
 		displayName = name;
-		if (!enabled && !GetComponent<Player>().clientController.isLocalPlayer)
+		if (!enabled && !player.clientController.isLocalPlayer)
 			enabled = true;
 	}
 }
